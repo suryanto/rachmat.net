2e6e48229b295d4fd11ff516300c6f5c176e4691
Suryanto Rachmat <suryanto@rachmat.net>
2017-03-19 11:46:41 +0800

    update template

--- web/themes/custom/rachmat/js/script.js
+++ web/themes/custom/rachmat/js/script.js
@@ -1,15 +1,30 @@
 (function ($, Drupal) {
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
-     $(context).foundation();
 
-     // Stellar Parallax
-     if ($.stellar != undefined) {
-       $.stellar({
-           horizontalScrolling: false,
-           responsive: true
-       });
-     }
+      $(context).foundation();
+
+      // smooth scroll for video anchor
+      $('#homepage-bottom-link', context).bind('tap', function (e) {
+
+        var $target = $($(this).attr('href'));
+
+        $('html, body').animate({
+          scrollTop: $target.offset().top
+        }, 300);
+
+        e.preventDefault();
+      });
+
+
+      // Stellar Parallax
+      if ($.stellar != undefined) {
+        $.stellar({
+          horizontalScrolling: false,
+          responsive: true
+        });
+      }
+
     }
   };
 })(jQuery, Drupal);
